c9a1b62b229fcda98a20567307e30253427211c1
Anna <apple@Anna-Cs-Air-4.socal.rr.com>
2023-09-12 14:40:54 -0700

    scraped all Women's data and unpacked json columns into separate columns

--- Data Analysis/ifsc_json_to_df.py
+++ Data Analysis/ifsc_json_to_df.py
@@ -2,85 +2,210 @@
 
 import pandas as pd
 import json
+import logging
+import re
 
 paths = [
-    '/Users/apple/Documents/PROGRAMMING/IFSC_climbing_world_cups/Data Analysis/ifsc_data/Wlead.json',
+    #'/Users/apple/Documents/PROGRAMMING/IFSC_climbing_world_cups/Data Analysis/ifsc_data/Wlead.json',
     # '/Users/apple/Documents/PROGRAMMING/IFSC_climbing_world_cups/Data Analysis/ifsc_data/Wboulder.json',
     # '/Users/apple/Documents/PROGRAMMING/IFSC_climbing_world_cups/Data Analysis/ifsc_data/Wcombined.json',
     # '/Users/apple/Documents/PROGRAMMING/IFSC_climbing_world_cups/Data Analysis/ifsc_data/Wboulderlead.json',
 ]
+dir = '/Users/apple/Documents/PROGRAMMING/IFSC_climbing_world_cups/Data Analysis/ifsc_data/'
 
-path = '/Users/apple/Documents/PROGRAMMING/IFSC_climbing_world_cups/Data Analysis/ifsc_data/Wlead_df.csv'
-out_path = '/Users/apple/Documents/PROGRAMMING/IFSC_climbing_world_cups/Data Analysis/ifsc_data/Wlead_ascents.csv'
+logging.basicConfig(
+    filename=dir + 'ifsc_results_scraper_log.txt',
+      level=logging.INFO, 
+      format='%(asctime)s:%(levelname)s:%(message)s'
+      )
 
+def cell_is_json(cell):
+    """
+    checks if a cell is a string containing json info
+    returns True/False
+    """
+    if not (isinstance(cell, list) or isinstance(cell, str)):  # Only process string cells
+        return False
+    
+    #logging.info(f"{cell} is type list and might be json")
+    try:
+        cell = str(cell)
+        cell = cell.replace("True", "true").replace("False", "false").replace("None","null").replace("\\xa","_")
+        cell = replace_single_quotes(cell)
 
-
-def normalize_depth(data, key_col, drop_col):
-    df_output = pd.DataFrame()
-
-    for index, row in data.iterrows():
-        # Check if key_col exists and is not null
+        if not (cell.startswith('{') and cell.endswith('}')) and not (cell.startswith('[') and cell.endswith(']')):
+            return False
         
-        if key_col in data.columns:
-            if row[key_col]:
-                if type(row[key_col]) == str:
-                    row_str = row[key_col].replace("'", '"').replace("True", "true").replace("False", "false").replace("None","null")
-                    print(type(row_str))
-                    print(row_str)
+        json.loads(cell)
+        return True
+    
+    except json.JSONDecodeError:
+        #logging.error(f"is_json function had json.JSONDecodeError for cell {cell}.")
+        return False
+    
+def unique_datatypes(df, column_name):
+    """
+    Identify unique datatypes in a given column of a DataFrame.
+
+    Parameters:
+    - df (pd.DataFrame): The DataFrame to inspect.
+    - column_name (str): The name of the column to check.
+
+    Returns:
+    - set: A set of unique datatypes in the specified column.
+    """
+    return set(df[column_name].map(type))
+
+def ifsc_json_to_csv(data, out_path):
+    """
+    data is df from pd.read_json(some_json_file)
+    """
+    ranking_depth_1 = pd.DataFrame()
+
+    try:
+        for _, row in data.iterrows():
+            # Normalize the 'ranking' list of dictionaries
+            df_temp = pd.json_normalize(row['results']['ranking'])
+            
+            # Add the 'comp_name' column to the temporary DataFrame
+            df_temp['comp_name'] = row['comp_name']
+            
+            # Append the temporary DataFrame to the ranking_depth_1 DataFrame
+            ranking_depth_1 = pd.concat([ranking_depth_1, df_temp], ignore_index=True)
+
+        ranking_depth_1.to_csv(out_path, index=False)
+
+        return ranking_depth_1 
+    
+    except Exception as e:
+        logging.error(f"ifsc_json_to_csv had error: \n {e}")
+
+def replace_single_quotes(str):
+    """
+    make string json-format before converting to json
+    """
+    return str.replace("{'", '{"').replace("': ", '": ').replace(": '", ': "').replace("', ", '", ').replace(", '", ', "').replace("'}", '"}')
+
+def normalize_depth(data, json_columns):
+    """
+    takes dataframe 'data' and list of str column names that has jsons as strings
+    returns dataframe with normalize/de-nested json as separate columns"""
+    df_output = pd.DataFrame()
+    length = len(data)
+    for idx, row in data.iterrows():
+        print(f"processing row {idx+1} of {length}")
+        
+        for json_col in json_columns:
+
+            if json_col not in data.columns or not row[json_col] or not cell_is_json(str(row[json_col])):
+                df_output = pd.concat([df_output, pd.DataFrame([row])], ignore_index=True)
+            else:
+                try:
+                    row_str = row[json_col].replace("True", "true").replace("False", "false").replace("None","null").replace("\\xa","_")
+                    row_str = replace_single_quotes(row_str)
                     row_json = json.loads(row_str)
-                    
+
                     df_temp = pd.json_normalize(row_json)
 
-                    row_data = row.drop(drop_col)
-
+                    row_data = row.drop(json_col)
+                    
                     for col, value in row_data.items():
                         df_temp[col] = value
 
                     df_output = pd.concat([df_output, df_temp], ignore_index=True)
-                else:
-                    print(type(row[key_col]))
-                    print(row[key_col])
-        else:
-            df_output = pd.concat([df_output, row], ignore_index=True)
+
+                except Exception as e:
+                    logging.error(f"row_str has type {type(row_str)} \n{row_str} \n Error: \n {e} \n")
+                    df_output = pd.concat([df_output, pd.DataFrame([row])], ignore_index=True)
+
+
+            # if json_col in data.columns:
+                
+            #     if row[json_col]:
+            #         if cell_is_json(str(row[json_col])):
+            #             try:
+            #                 row_str = row[json_col].replace("True", "true").replace("False", "false").replace("None","null").replace("\\xa","_")
+            #                 row_str = replace_single_quotes(row_str)
+            #                 row_json = json.loads(row_str)
+                            
+            #                 df_temp = pd.json_normalize(row_json)
+
+            #                 row_data = row.drop(json_col)
+
+            #                 for col, value in row_data.items():
+            #                     df_temp[col] = value
+
+            #                 df_output = pd.concat([df_output, df_temp], ignore_index=True)
+                        
+            #             except Exception as e:
+            #                 logging.error(f"row_str has type {type(row_str)} \n{row_str} \n Error: \n {e} \n")
+
+            # else:
+            #     df_output = pd.concat([df_output, row], ignore_index=True)
 
     return df_output
 
-#data = pd.read_json(path)
+def main():
+    category = 'Wboulderlead'
 
-# ranking_depth_1 = normalize_depth(data, 'results.ranking', 'results')
-# print(ranking_depth_1.head())
+    #1 json to csv unpacking Results dict -> {category}_initial.csv
+    # category_json = pd.read_json(dir + category + '.json') #e.g. Wcombined.json
+    # data = ifsc_json_to_csv(category_json, dir + category + '_initial.csv')
+    # print(data.head())
 
-# round_depth_2 = normalize_depth(ranking_depth_1, 'rounds', 'rounds')
-# print(round_depth_2.head())
+    #2 unpacking Rounds dict -> {category}_midway.csv
+    # data = pd.read_csv(dir + category + "_initial.csv")
 
-# ascents_depth_3 = normalize_depth(round_depth_2, 'ascents', 'ascents')
+    #3 unpacking Ascents dict from _midway.csv-> {category}.csv
+    data = pd.read_csv(dir + category + "_midway.csv")
+
+
+    #checks for all data
+    print(data.head())
+    unique_dtypes_dict = {
+        col: unique_datatypes(data, col) for col in data.columns
+    }
+    logging.info(f"unique dtypes of each column are {unique_dtypes_dict}")
+
+
+    json_cells = data.map(cell_is_json, na_action='ignore')
+
+    # gives both parent and nested/child columns
+    all_json_columns = json_cells.columns[json_cells.any()].tolist()
+
+    # only child columns
+    json_columns = [col for col in all_json_columns if not any(c.startswith(f"{col}.") for c in all_json_columns)]
+    logging.info(f"json columns in str type needing unpacking are {json_columns}")
+
+
+
+    #2
+    # round_depth_2 = normalize_depth(data, json_columns)
+    # round_depth_2.to_csv(dir + category + '_midway.csv')
+
+
+    # #3
+    ascents_depth_3 = normalize_depth(data, json_columns)
+    print(ascents_depth_3.head())
+    ascents_depth_3.to_csv(dir + category + '.csv')
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+# data = pd.read_csv(path)
+
+# ascents_depth_3 = normalize_depth(data, 'ascents', 'ascents')
 # print(ascents_depth_3.head())
 
-
-data = pd.read_csv(path)
-
-ascents_depth_3 = normalize_depth(data, 'ascents', 'ascents')
-print(ascents_depth_3.head())
-
-ascents_depth_3.to_csv(out_path, index=False)
+# ascents_depth_3.to_csv(out_path, index=False)
 
 
 
 
-
-# # Create an empty DataFrame to hold the normalized results
-# ranking_depth_1 = pd.DataFrame()
-
-# # Iterate over each row in the data
-# for index, row in data.iterrows():
-#     # Normalize the 'ranking' list of dictionaries
-#     df_temp = pd.json_normalize(row['results']['ranking'])
-    
-#     # Add the 'comp_name' column to the temporary DataFrame
-#     df_temp['comp_name'] = row['comp_name']
-    
-#     # Append the temporary DataFrame to the ranking_depth_1 DataFrame
-#     ranking_depth_1 = pd.concat([ranking_depth_1, df_temp], ignore_index=True)
 
 # print(ranking_depth_1.head())
 
